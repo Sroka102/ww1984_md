00a88be5856a70de8583101ba5ad8294325a6f0c
Sroka102 <36758797+Sroka102@users.noreply.github.com>
2021-09-19 18:09:53 +0200

    Add files via upload

---
+++ ww1984.ts
@@ -0,0 +1,159 @@
+// enums
+enum Direction {
+    //% block="do Przodu"
+    Forward,
+    //% block="do Tyłu"
+    Back,
+    //% block="w Lewo"
+    Left,
+    //% block="w Prawo"
+    Right
+}
+
+enum BeamsGlass {
+    //% blockIdentity="blocks.block" enumval=262385 block="Yellow Stained Glass"
+    //% jres alias=YELLOW_STAINED_GLASS
+    YellowStainedGlass = 262385,
+    //% blockIdentity="blocks.block" enumval=327921 block="Lime Stained Glass"
+    //% jres alias=LIME_STAINED_GLASS
+    LimeStainedGlass = 327921,
+    //% blockIdentity="blocks.block" enumval=721137 block="Blue Stained Glass"
+    //% jres alias=BLUE_STAINED_GLASS
+    BlueStainedGlass = 721137,
+    //% blockIdentity="blocks.block" enumval=917745 block="Red Stained Glass"
+    //% jres alias=RED_STAINED_GLASS
+    RedStainedGlass = 917745
+}
+
+// global variables
+const stopBlock = BEDROCK
+const stopPosition = world(35,1,0)
+const locatePaintingTarget = 14
+const locateGoonTarget = 113
+
+const directions = [
+    FORWARD,
+    BACK,
+    LEFT,
+    RIGHT
+];
+
+const turns = [
+    LEFT_TURN,
+    RIGHT_TURN
+]
+
+//%  block="Wonder Woman" weight=200 color=#BF9B30 icon="\u2605"
+namespace ww {
+
+    /**
+     * Wonder Woman przejdzie n kroków w kierunku d
+     */
+    //% block="Idź %d %n kroków"
+    export function moveWW(d: Direction, n: number): void {
+        for (let i = 0; i < n; i++){
+            if(shouldStop()) return;
+
+            const direction = directions[d];
+
+            agent.move(direction, 1);
+        }
+    }
+
+    /**
+     * Obraca Wonder Woman w kierunku t
+     */
+    //% block="Obróć %t"
+    export function turnWW(t: TurnDirection): void {
+        if(shouldStop()) return;
+
+        const turn = turns[t];
+
+        agent.turn(turn);
+    }  
+
+    /**
+     * Umieszcza blok w podanym kierunku d
+     * @param block the block
+     */    
+    //% block="Umieść %block blok szkła %d"
+    export function placeBlock(block: BeamsGlass, d: Direction): void {
+        if(shouldStop()) return;
+
+        agent.setItem(block, 1, 1)
+        agent.setSlot(1)
+
+        const direction = directions[d];
+
+        agent.place(direction);
+    }  
+
+    /**
+     * Sprawdza występowanie obrazu w ustalonym kierunku d
+     */
+    //% block="obraz w skrzyni %d"
+    export function locatePainting(d: Direction): boolean {
+        if(shouldStop()) return false;
+
+        const direction = directions[d];
+
+        const inspected = agent.inspect(AgentInspection.Block, direction);
+
+        return inspected === locatePaintingTarget;
+    }
+
+    /**
+     * Rozbija blok w ustalonym kierunku d
+     */
+    //% block="Rozbij skrzynię %d"
+    export function retrievePainting(d: Direction): void {
+        if(shouldStop()) return;
+
+        const direction = directions[d];
+
+        agent.destroy(direction);
+    }    
+
+    /**
+     * Sprawdza występowanie zbira w ustalonym kierunku d
+     */
+    //% block="uczestnik jest złodziejem %d"
+    export function locateGoon(d: Direction): boolean {
+        if(shouldStop()) return false;
+
+        const direction = directions[d];
+
+        const inspected = agent.inspect(AgentInspection.Block, direction);
+
+        return inspected === locateGoonTarget;
+    }
+
+    /**
+     * Używa lassa do złapania postaci w ustalonym kierunku d
+     */
+    //% block="Łap lassem złodzieja %d"
+    export function apprehendGoon(d: Direction): void {
+        if(shouldStop()) return;
+
+        const direction = directions[d];
+
+        agent.destroy(direction);
+    }
+
+    /**
+     * Bezszelestnie atakuje postać w ustalonym kierunku d
+     */
+    //% block="Atakuj przestępcę %d"
+    export function takedownGoon(d: Direction): void {
+        if(shouldStop()) return;
+
+        const direction = directions[d];
+
+        agent.destroy(direction);
+    }    
+
+    // helper functions
+    function shouldStop(): boolean {
+        return blocks.testForBlock(stopBlock, stopPosition);
+    }
+}
